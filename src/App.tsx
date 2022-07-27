084d1c45d677d7307b414a2de7f141d668d94ba8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-27 19:49:30 +0100

    add switcher component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Loader } from "./Components/Loader";
+import { ThemeSwitcher } from "./Components/<ThemeSwitcher";
 
 function App() {
   return (
     <>
       <Loader />
+      <ThemeSwitcher />
     </>
   );
 }
