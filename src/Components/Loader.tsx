43ce091d2c567f594b261b39c8c784472b481819
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-27 22:27:11 +0100

    add intial and exit animations

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,12 +1,46 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 export const Loader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 3000);
   return (
-    <div className="loader_wrapper">
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="shadow"></div>
-      <div className="shadow"></div>
-      <div className="shadow"></div>
-    </div>
+    <AnimatePresence>
+      {isLoading && (
+        <div className="loader_wrapper">
+          <motion.div
+            key={1}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3, duration: 1.5 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="circle circle_1"></div>
+            <div className="shadow shadow_1"></div>
+          </motion.div>
+          <motion.div
+            key={2}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.9, duration: 1.7 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="circle circle_2"></div>
+            <div className="shadow shadow_2"></div>
+          </motion.div>
+          <motion.div
+            key={3}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 2 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="circle circle_3"></div>
+            <div className="shadow shadow_3"></div>
+          </motion.div>{" "}
+        </div>
+      )}
+    </AnimatePresence>
   );
 };
