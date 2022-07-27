bdcc38ca871042c6f5ed7fa31a7adea77756a810
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-27 18:43:11 +0100

    initial commit

---
+++ src/Components/Loader.tsx
@@ -0,0 +1,12 @@
+export const Loader = () => {
+  return (
+    <div className="loader_wrapper">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+    </div>
+  );
+};
