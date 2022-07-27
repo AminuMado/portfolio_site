fa96ae3cddf78b81721d778ab81d4ac560b01244
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-27 18:44:17 +0100

    add animation style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bounce: {
+          "0%": {
+            top: "60px",
+            height: "5px",
+            borderRadius: "50px 50px 25px 25px",
+            transform: "scaleX(1.7)",
+          },
+          "40%": {
+            height: "20px",
+            borderRadius: "50%",
+            transform: "scaleX(1)",
+          },
+          "100%": { top: "0%" },
+        },
+      },
+      animation: { "loading-bounce": "bounce 0.5s ease infinite alternate" },
+    },
   },
   plugins: [],
 };
+
+// 0% {
+//   top: 60px;
+//   height: 5px;
+//   border-radius: 50px 50px 25px 25px;
+//   transform: scaleX(1.7);
+// }
+// 40% {
+//   height: 20px;
+//   border-radius: 50%;
+//   transform: scaleX(1);
+// }
+// 100% {
+//   top: 0%;
+// }
