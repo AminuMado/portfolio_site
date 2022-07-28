8f45bab4d23bce6ecb13ce396281e233b1f2dedb
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-28 16:47:53 +0100

    initial commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from "react";
+
+type Props = {
+  title: string;
+  center: boolean;
+};
+
+const Header = ({ title, center }: Props): ReactElement => {
+  return (
+    <header className="grid grid-cols-1 w-full p-10 text-primary-color">
+      <div className="flex items-center w-full">
+        {center && (
+          <div className="flex-shrink flex-grow h-px bg-loading-color" />
+        )}
+        <h1 className="mx-10 text-3xl">{title}</h1>
+        <div className="flex-shrink flex-grow h-px bg-loading-color" />
+        <hr />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
