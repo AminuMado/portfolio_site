24f1fcd725ab01e60f27c4e5163e87a69b34f734
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-29 12:03:00 +0100

    update styling

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = (): ReactElement => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="text-primary-color w-full mt-10 text-xl md:text-2xl px-8 ">
-      <div className=" flex flex-col md:flex-row md:gap-8 flex-wrap justify-center items-center border-t-[1px] py-11 px-8">
+      <div className=" flex flex-col md:flex-row md:gap-8 flex-wrap justify-center items-center border-t-[1px] border-primary-color py-11 px-8">
         <span>© {currentYear} AminuMado </span>
         <Loader2 />
         <span>All rights reserved.</span>
