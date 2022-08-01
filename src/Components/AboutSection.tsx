cdb1f21e848e0493b1aa92343e66f7365a3e0d92
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-01 12:19:00 +0100

    initial commit

---
+++ src/Components/AboutSection.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import { Header } from "./Header";
+
+export const AboutSection = (): ReactElement => {
+  return (
+    <section className="grid grid-cols-1 w-full p-10 text-primary-color"></section>
+  );
+};
