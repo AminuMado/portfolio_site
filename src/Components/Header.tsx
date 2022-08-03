aa3ee08c809e81569a94e87cacdd799c8e2eff45
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-03 22:09:20 +0100

    update styling:add responsiveness at all screen sizes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -12,7 +12,9 @@ export const Header = ({ title, center }: Props): ReactElement => {
         {center && (
           <div className="flex-shrink flex-grow h-px bg-loading-color" />
         )}
-        <h1 className="mx-10 text-3xl">{title}</h1>
+        <h1 className="mx-10 my-5  text-2xl sm:text-3xl md:text-5xl">
+          {title}
+        </h1>
         <div className="flex-shrink flex-grow h-px bg-loading-color" />
       </div>
     </header>
