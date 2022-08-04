850a0309229bfeb05cceeb1cd4303bd86ac90286
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-04 17:17:37 +0100

    change single quotes to double quotes

--- src/Components/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher.tsx
@@ -42,7 +42,7 @@ export const ThemeSwitcher = ({ theme, setTheme }: ThemeSwitcherProps) => {
 
   return (
     <DarkModeSwitch
-      className="fixed right-4 top-1"
+      className="fixed right-4 top-1 z-10"
       checked={darkMode}
       onChange={toggleDarkMode}
       size={40}
