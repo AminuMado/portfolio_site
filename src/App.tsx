de80f3f0072d4ecf96c43854b17184142f62909e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-04 17:18:10 +0100

    import page components

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,26 @@ import { useState } from "react";
 import { getInitialTheme } from "./Utils";
 import { ContactSection } from "./Components/ContactSection";
 import { Footer } from "./Components/Footer";
-import { Loader } from "./Components/Loader";
 import { SkillsSection } from "./Components/SkillsSection";
-import { ThemeSwitcher } from "./Components/ThemeSwitcher";
 import { ProjectsSection } from "./Components/ProjectsSection";
 import { AboutSection } from "./Components/AboutSection";
 import { Landing } from "./Components/Landing";
 
 function App() {
   const [theme, setTheme] = useState(getInitialTheme());
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
-      <Loader />
-      <Landing />
-      <ThemeSwitcher theme={theme} setTheme={setTheme} />
-      <AboutSection />
-      <SkillsSection theme={theme} />
-      <ProjectsSection />
-      <ContactSection />
-      <Footer />
+      <Landing setIsLoading={setIsLoading} theme={theme} setTheme={setTheme} />
+      {!isLoading && (
+        <>
+          <AboutSection />
+          <SkillsSection theme={theme} />
+          <ProjectsSection />
+          <ContactSection />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
